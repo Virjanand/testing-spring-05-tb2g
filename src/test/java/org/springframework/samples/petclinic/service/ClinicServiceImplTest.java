4e7eb445ec169427098b14fcd5e85c1f8e283419
Virjanand <virjanand@virjanand.com>
2020-08-01 08:18:13 +0200

    add unit test for ClinicServiceImpl

---
+++ src/test/java/org/springframework/samples/petclinic/service/ClinicServiceImplTest.java
@@ -0,0 +1,39 @@
+package org.springframework.samples.petclinic.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.samples.petclinic.model.PetType;
+import org.springframework.samples.petclinic.repository.PetRepository;
+
+import java.util.Collection;
+import java.util.List;
+
+import static java.util.Collections.singletonList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class ClinicServiceImplTest {
+
+    @Mock
+    PetRepository petRepository;
+
+    @InjectMocks
+    private ClinicServiceImpl clinicService;
+
+    @Test
+    void findPetTypes() {
+        //given
+        List<PetType> petTypes = singletonList(new PetType());
+        given(petRepository.findPetTypes()).willReturn(petTypes);
+
+        //when
+        Collection<PetType> actualPetTypes = clinicService.findPetTypes();
+
+        //then
+        assertThat(actualPetTypes).isEqualTo(petTypes);
+    }
+}
