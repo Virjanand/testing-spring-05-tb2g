98caf2dd5a279313f6e8b080e1a421d64e76134b
Virjanand <virjanand@virjanand.com>
2020-08-06 06:12:46 +0200

    Create classes to test with SpringContext

---
+++ src/main/java/org/springframework/samples/petclinic/sfg/HearingInterpreter.java
@@ -0,0 +1,17 @@
+package org.springframework.samples.petclinic.sfg;
+
+public class HearingInterpreter {
+    private final WordProducer wordProducer;
+
+    public HearingInterpreter(WordProducer wordProducer) {
+        this.wordProducer = wordProducer;
+    }
+
+    public String whatIHeard() {
+        String word = wordProducer.getWord();
+
+        System.out.println(word);
+
+        return word;
+    }
+}
---
+++ src/main/java/org/springframework/samples/petclinic/sfg/LaurelWordProducer.java
@@ -0,0 +1,8 @@
+package org.springframework.samples.petclinic.sfg;
+
+public class LaurelWordProducer implements WordProducer {
+    @Override
+    public String getWord() {
+        return "Laurel";
+    }
+}
