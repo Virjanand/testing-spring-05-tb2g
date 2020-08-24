84a3fe54cb120997c557b7585bc5d9b1b4b9fcb3
Virjanand <virjanand@virjanand.com>
2020-08-24 09:16:10 +0200

    Assignment: Get laurel from property with component scan

--- src/main/java/org/springframework/sfg/PropertiesWordProducer.java
+++ src/main/java/org/springframework/sfg/PropertiesWordProducer.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("externalized")
+@Profile({"externalized", "laurel-property"})
 @Primary
 public class PropertiesWordProducer implements WordProducer{
 
--- src/test/java/org/springframework/sfg/junit5/HearingInterpreterLaurelPropertySourceTest.java
+++ src/test/java/org/springframework/sfg/junit5/HearingInterpreterLaurelPropertySourceTest.java
@@ -10,27 +10,26 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@ActiveProfiles("externalized")
-@SpringJUnitConfig(classes = HearingInterpreterPropertySourceTest.TestConfig.class)
-@TestPropertySource("classpath:yanny.properties")
-class HearingInterpreterPropertySourceTest {
 
-    @Profile("externalized")
+@ActiveProfiles("laurel-property")
+@SpringJUnitConfig(classes = HearingInterpreterLaurelPropertySourceTest.TestConfig.class)
+@TestPropertySource("classpath:laurel.properties")
+public class HearingInterpreterLaurelPropertySourceTest {
+
     @Configuration
     @ComponentScan("org.springframework.sfg")
     static class TestConfig {
-
     }
 
     @Autowired
-    HearingInterpreter hearingInterpreter;
+    private HearingInterpreter hearingInterpreter;
 
     @Test
     void whatIHeard() {
         String word = hearingInterpreter.whatIHeard();
 
-        assertEquals("YaNNy", word);
+        assertEquals("LaureL", word);
     }
 }
