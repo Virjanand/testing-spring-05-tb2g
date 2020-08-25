60c5cf0e28ce0cf8cec4aee948793114bc4693e5
Virjanand <virjanand@virjanand.com>
2020-08-25 20:14:04 +0200

    optimize imports

--- src/test/java/org/springframework/sfg/junit5/HearingInterpreterLaurelPropertySourceTest.java
+++ src/test/java/org/springframework/sfg/junit5/HearingInterpreterLaurelPropertySourceTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.sfg.HearingInterpreter;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
